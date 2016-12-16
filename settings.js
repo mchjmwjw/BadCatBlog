c642e070cce671161b7021e62d39844c3e03a108
mchjmwjw <mchjmwjw@163.com>
2016-12-16 20:57:41 +0800

    修改数据库

--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
+var dburi = process.env.MONGOLAB_URI;
+
 module.exports = {
   cookieSecret: 'myblog',
-  db: 'blog',
-  host: 'localhost',
-  port: 27017
+  db: dburi.substring(dburi.lastindexof("/") + 1),
+  host: dburi.substring(dburi.indexOf("@") + 1, dburi.lastindexOf(":")),
+  port: dburi.substring(dburi.lastindexOf(":") + 1, dburi.lastindexOf("/"))
 };
