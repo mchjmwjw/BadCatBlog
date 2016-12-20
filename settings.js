fe0fe95aac08adb452cf401979cb625ff76c7295
mchjmwjw <mchjmwjw@163.com>
2016-12-20 13:55:29 +0800

    读取Mongogo_URI

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ var dburi = process.env.MONGOLAB_URI;
 
 module.exports = {
   cookieSecret: 'myblog',
-  db: dburi.substring(dburi.lastindexof("/") + 1),
-  host: dburi.substring(dburi.indexOf("@") + 1, dburi.lastindexOf(":")),
-  port: dburi.substring(dburi.lastindexOf(":") + 1, dburi.lastindexOf("/"))
+  db: dburi.dbname,//substring(dburi.lastindexof("/") + 1),
+  host: dburi.host,//substring(dburi.indexOf("@") + 1, dburi.lastindexOf(":")),
+  port: dburi.port//substring(dburi.lastindexOf(":") + 1, dburi.lastindexOf("/"))
 };
